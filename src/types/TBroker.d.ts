cce96cb24247e28e562e5cec830aa8978753bfd3
Helena Machado <helena.machado.hm@gmail.com>
2022-04-14 02:55:13 -0300

    :label: feat: add broker types

---
+++ src/types/TBroker.d.ts
@@ -0,0 +1,13 @@
+type TBroker = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  cpf: string;
+  creci: string;
+  creciState: string;
+  creciExp: any;
+  city: string;
+  isActive: boolean;
+};
