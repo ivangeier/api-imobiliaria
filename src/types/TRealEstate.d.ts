71bed7185e8a147c7ab5dcbf0b19df581586e631
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-04-13 22:27:08 -0300

    🏷️ add real estate and user types globally

---
+++ src/types/TRealEstate.d.ts
@@ -0,0 +1,11 @@
+type TRealEstate = {
+  id: string;
+  name: string;
+  cnpj: string;
+  city: string;
+  initialBroker: string;
+  initialProperties: string;
+  properties: string;
+  broker: string;
+  role: string;
+};
---
+++ src/types/TUser.d.ts
@@ -0,0 +1,9 @@
+type TUser = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  cpf: string;
+  email: string;
+  password: string;
+  role: string;
+};
