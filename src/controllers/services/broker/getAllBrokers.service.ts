2b83bbe2f8292515a12e79d9110cb8a19080c671
ivangeier <vilajr.ivan@gmail.com>
2022-04-17 16:52:23 -0300

    ✨ create new route - getAllBrokers

---
+++ src/controllers/services/broker/getAllBrokers.service.ts
@@ -0,0 +1,20 @@
+import Broker from "../../../models/Broker.model";
+import User from "../../../models/User.model";
+
+export default async function getAllBrokers(realEstateId) {
+   const brokers = await Broker.findAll({
+      where: {
+         realEstateId,
+      },
+      include: {
+         model: User,
+         attributes: {
+            exclude: ["password"],
+         }
+      },
+   });
+   
+   return brokers.map((broker) => {
+      return broker.get();
+   });
+   }
--- src/routes/Broker.routes.ts
+++ src/routes/Broker.routes.ts
@@ -1,9 +1,13 @@
-import { Express } from "express";
-import { BrokerController } from "../controllers/Broker.controller";
+import { Express } from 'express';
+import { BrokerController } from '../controllers/Broker.controller';
+import authMiddleware from '../middlewares/auth';
+
+const auth = authMiddleware();
 
 export const brokerRoutes = (app: Express) => {
-  app.post("/broker/register", BrokerController.create);
-  app.delete("/broker/delete", BrokerController.deleteOne);
-  app.get("/broker", BrokerController.getById);
-  app.put("/broker/update", BrokerController.update);
+  app.get('/brokers', BrokerController.getAll);
+  app.post('/broker/register', auth.authenticate(), BrokerController.create);
+  app.delete('/broker/delete', BrokerController.deleteOne);
+  app.get('/broker', BrokerController.getById);
+  app.put('/broker/update', BrokerController.update);
 };
