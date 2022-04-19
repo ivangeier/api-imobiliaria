02c0656aeb4a973aec2332cfd5cbb3f6134a0921
ivangeier <vilajr.ivan@gmail.com>
2022-04-18 21:42:17 -0300

    🔐 Add authentication on route /brokers

--- src/routes/Broker.routes.ts
+++ src/routes/Broker.routes.ts
@@ -5,7 +5,7 @@ import authMiddleware from '../middlewares/auth';
 const auth = authMiddleware();
 
 export const brokerRoutes = (app: Express) => {
-  app.get('/brokers', BrokerController.getAll);
+  app.get('/brokers', auth.authenticate(), BrokerController.getAll);
   app.post('/broker/register', auth.authenticate(), BrokerController.create);
   app.delete('/broker/delete', BrokerController.deleteOne);
   app.get('/broker', BrokerController.getById);
