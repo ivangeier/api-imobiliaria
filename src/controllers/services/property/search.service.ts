a7b0d7a56a5305d1f6d78e87401299f53050c96a
ivangeier <vilajr.ivan@gmail.com>
2022-04-26 20:00:36 -0300

    🔧 improve search filters

--- src/controllers/services/property/search.service.ts
+++ src/controllers/services/property/search.service.ts
@@ -1,4 +1,5 @@
 import { Op } from "sequelize";
+import RealEstate from "../../../models/RealEstate.model";
 import Property from "../../../models/Property.model";
 
 export default async function getFilteredProperties(queries: TPropertyQuery) {
@@ -9,12 +10,16 @@ export default async function getFilteredProperties(queries: TPropertyQuery) {
       isActive: true,
       ...(mode === 'aluguel' ? {isRenting: true} : mode === 'venda' ? {isSelling: true} : {}),
       ...(min && max ? mode === 'aluguel' ? {valueRental: {[Op.between]: [min, max]}} : {valueSell: {[Op.between]: [min, max]}} : {}),
-      ...(beds ? {amountBedrooms: {[Op.gte]: beds}} : {}),
-      ...(baths ? {amountBathrooms: {[Op.gte]: baths}} : {}),
-      ...(garage ? {amountGarage: {[Op.gte]: garage}} : {}),
+      ...(beds ? {amountBedrooms: {[Op.eq]: beds}} : {}),
+      ...(baths ? {amountBathrooms: {[Op.eq]: baths}} : {}),
+      ...(garage ? {amountGarage: {[Op.eq]: garage}} : {}),
       ...(type === 'apartamento' ? {type: 'apartamento'} : type === 'casa' ? {type: 'casa'} : {}),
       ...(state ? {state: state} : {}),
      },
+     include: {
+      model: RealEstate,
+      attributes: ["name"]
+    },
      order:[
       ['updatedAt', 'DESC']
       ]  
